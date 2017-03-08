35525732711e5ee38485a8968c76af5df34e4302
dr.dimitru <ceo@veliov.com>
2017-03-08 20:42:39 +0300

    v1.1.0
     - No breaking changes for Node.js (NPM) usage
     - Breaking for Meteor.js - `Spiderable` variable isn't pushed to
    global scope use `import`
     - ES6 + modules + import for Meteor.js
     - Move codebase away from CoffeeScript
     - Dependencies update
     - Overall minor enhancements

--- package.js
+++ package.js
@@ -1,23 +1,23 @@
 Package.describe({
   name: 'ostrio:spiderable-middleware',
-  version: '1.0.2',
+  version: '1.1.0',
   summary: 'Allow your JavaScript website to be crawled perfectly by search engines.',
   git: 'https://github.com/VeliovGroup/spiderable-middleware',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.1');
-  api.addFiles('./lib/meteor.js', 'server');
-  api.export('Spiderable');
+  api.versionsFrom('1.4');
+  api.use('ecmascript', 'server');
+  api.mainModule('./lib/meteor.js', 'server');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
+  api.use(['tinytest', 'ecmascript']);
   api.use(['ostrio:spiderable-middleware', 'underscore'], 'server');
   api.addFiles('./lib/meteor-tests.js', 'server');
 });
 
 Npm.depends({
-  'spiderable-middleware': '1.0.2'
+  'spiderable-middleware': '1.1.0'
 });
