013fe630df54f2b364b53b9b7f550b3e96598706
dr.dimitru <ceo@veliov.com>
2017-06-02 03:52:55 +0300

    v1.2.0
     - Overall codebase enhancements and performance improvements
     - Compatibility with `meteor@1.5`
     - Update NPM dependencies
     - Update examples in the docs

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:spiderable-middleware',
-  version: '1.1.0',
+  version: '1.2.0',
   summary: 'Allow your JavaScript website to be crawled perfectly by search engines.',
   git: 'https://github.com/VeliovGroup/spiderable-middleware',
   documentation: 'README.md'
@@ -9,15 +9,15 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
   api.use('ecmascript', 'server');
-  api.mainModule('./lib/meteor.js', 'server');
+  api.mainModule('lib/meteor.js', 'server');
 });
 
 Package.onTest(function(api) {
   api.use(['tinytest', 'ecmascript']);
   api.use(['ostrio:spiderable-middleware', 'underscore'], 'server');
-  api.addFiles('./lib/meteor-tests.js', 'server');
+  api.addFiles('lib/meteor-tests.js', 'server');
 });
 
 Npm.depends({
-  'spiderable-middleware': '1.1.0'
+  'spiderable-middleware': '1.2.0'
 });
