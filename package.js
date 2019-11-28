2ab5503555b8d4497e3e26855ab2375000823344
dr.dimitru <ceo@veliov.com>
2019-11-28 03:59:52 +0300

    🚢 v1.4.4
    __Changed:__
    
    - 👷‍♂️ List of "bad http statuses" (*which would cause request retry*)
    to `[502, 503, 504, 599]`; *was `[ 300, 303, 305, 400, 407, 408, 409,
    410] && >= 500`*
    - 👨‍💻 Retry delay between retries to `64ms`; *was `256ms`*
    - 👷‍♂️ Added `Accept: */*` request header
    
    __Other changes:__
    
    - 👷‍♂️ Overall *minor* codebase enhancements and performance
    improvements
    - 📦 Optimized package bundle size
    - 🤝 Compatibility with `meteor@1.8.2`
    
    __Updates:__
    
    - 📦 `request-libcurl@2.0.3`; *was `v2.0.0`*
    - 📦 [dev-dependency] `mocha@6.2.2`; *was `v6.2.1`*
    - 📦 Internal meteor dependencies update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:spiderable-middleware',
-  version: '1.4.3',
+  version: '1.4.4',
   summary: 'Allow your JavaScript website to be crawled perfectly by search engines.',
   git: 'https://github.com/VeliovGroup/spiderable-middleware',
   documentation: 'README.md'
@@ -19,5 +19,5 @@ Package.onTest((api) => {
 });
 
 Npm.depends({
-  'request-libcurl': '2.0.0'
+  'request-libcurl': '2.0.3'
 });
