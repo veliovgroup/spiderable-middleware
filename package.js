dd21609efb89bb6d291dcce8c615d2048372d87d
dr.dimitru <ceo@veliov.com>
2020-05-04 02:26:58 -0400

    📦 v1.5.1
    - 👷 This is maintenance release;
    - 🤝 Compatibility with `meteor@1.10.2`;
    - 📦 Update `request-libcurl` NPM dependency to `v2.2.0`, *was
    `v2.0.6`*;
    - 📦 Update dev-dependencies.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'ostrio:spiderable-middleware',
-  version: '1.5.0',
+  version: '1.5.1',
   summary: 'Allow JavaScript websites to be perfectly crawled and indexed by search engines (SEO)',
   git: 'https://github.com/VeliovGroup/spiderable-middleware',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.6.1');
+  api.versionsFrom('1.9.1');
   api.use('ecmascript', 'server');
   api.mainModule('lib/index.js', 'server');
 });
@@ -19,5 +19,5 @@ Package.onTest((api) => {
 });
 
 Npm.depends({
-  'request-libcurl': '2.0.6'
+  'request-libcurl': '2.2.0'
 });
