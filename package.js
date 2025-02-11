211d9f0163de7b5ed386f345f7c0bf7bbb1394f2
dr.dimitru <ceo@veliov.com>
2025-02-11 22:34:54 +0200

    📦 v2.1.0
    - 👨‍💻 Refactored codebase
    - 🚀 Enhancements to improve response time and TTFB
    - 👨‍🔧 Improved debugging with new `{debug: true}` option and `DEBUG=true` env.var support
    - 👷‍♂️ Improved test-suites, updated to the latest releases
    - 📦 `engines.node: >=7.1.0`
    - 📔 Improved and refactored documentation
    - ☄️ Tested and supported the latest `meteor@3.1.*` release

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'ostrio:spiderable-middleware',
-  version: '2.0.0',
-  summary: 'Allow JavaScript websites to be perfectly crawled and indexed by search engines (SEO)',
+  version: '2.1.0',
+  summary: 'Ensure Meteor.js apps are flawlessly crawled and indexed by search engines for optimal SEO',
   git: 'https://github.com/veliovgroup/spiderable-middleware',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.9.1');
+  api.versionsFrom(['1.9.1', '2.1', '3.0.1']);
   api.use('ecmascript', 'server');
   api.mainModule('lib/index.js', 'server');
 });
