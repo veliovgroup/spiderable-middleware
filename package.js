10f21f854b12b0811924831da2d7ba47791728e3
dr.dimitru <ceo@veliov.com>
2025-02-14 23:46:22 +0200

    📦 v2.2.0
    Critical:
    
    - ⚠️ `.handle` now returns `void` (*was: `boolean`*); This needed to match TS typing for express, connect, and other server-routing middleware registers
    
    Major:
    
    - 📔 Now `.handle` is recommended middleware for all integrations
    - 👨‍💻 Now `.handle` and `.handler` middlewares has fixed context, meaning that it doesn't require calling `.bind()` no more
    
    Other changes:
    
    - 👨‍🔧 Fix #29, thanks to @StorytellerCZ
    - 👨‍💻 Added types and TS support in NPM and Atmosphere packages
    - 👨‍💻 Fixed context binding in `.handle` and `.handler` middlewares
    - 👷‍♂️ Improved tests, added TS tests
    - 📔 Improved docs, examples, and API description

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:spiderable-middleware',
-  version: '2.1.0',
+  version: '2.2.0',
   summary: 'Ensure Meteor.js apps are flawlessly crawled and indexed by search engines for optimal SEO',
   git: 'https://github.com/veliovgroup/spiderable-middleware',
   documentation: 'README.md'
@@ -10,10 +10,13 @@ Package.onUse((api) => {
   api.versionsFrom(['1.9.1', '2.1', '3.0.1']);
   api.use('ecmascript', 'server');
   api.mainModule('lib/index.js', 'server');
+
+  api.use(['zodern:types@1.0.13', 'typescript'], 'server', { weak: true });
+  api.addAssets('types/index.d.ts', 'server');
 });
 
 Package.onTest((api) => {
-  api.use(['tinytest', 'ecmascript']);
-  api.use(['underscore', 'http', 'webapp'], 'server');
-  api.addFiles('test/meteor.js', 'server');
+  api.use(['tinytest', 'ecmascript', 'typescript', 'zodern:types']);
+  api.use(['underscore', 'http', 'webapp', 'ostrio:spiderable-middleware'], 'server');
+  api.addFiles(['test/meteor.js', 'test/meteor.ts'], 'server');
 });
